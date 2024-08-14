7b4bcb8b861ebe810082416e248fcc06e5e5af76
Aydan McGah <amcgah@cainc.com>
2024-08-14 15:49:46 -0400

    fully up to date

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -9,8 +9,20 @@ export function Pages() {
   let popUpContent = null;
 
   function handleClickPageCircle(event) {
-    setShowPopUp(true);
-    setActivePopUp(event.target.id);
+    const clickedId = event.target.id;
+
+    if (clickedId === "fullresume") {
+      // If "Full Resume" is clicked, close all pop-ups
+      setShowPopUp(false);
+      setActivePopUp("");
+      const resumeLink =
+        "https://drive.google.com/file/d/1YBhGyd2uvTaqZp40geLOAvHsb8U0BF2_/view?usp=drive_link";
+      window.open(resumeLink);
+    } else {
+      // For other PageCircles, open pop-up for the clicked item
+      setActivePopUp(clickedId);
+      setShowPopUp(true);
+    }
   }
 
   function handleClosePopUp() {
@@ -43,9 +55,6 @@ export function Pages() {
           text="Full Resume"
           id="fullresume"
           onClick={handleClickPageCircle}
-          link={
-            "https://drive.google.com/file/d/1YBhGyd2uvTaqZp40geLOAvHsb8U0BF2_/view?usp=drive_link"
-          }
           activePopUp={activePopUp}
         />
         {popUpContent}
@@ -56,3 +65,62 @@ export function Pages() {
     </>
   );
 }
+
+// import React, { useState } from "react";
+// import { PopUp } from "./PopUp";
+// import { PagesCircle } from "./PagesCircle";
+
+// export function Pages() {
+//   const [showPopUp, setShowPopUp] = useState(false);
+//   const [activePopUp, setActivePopUp] = useState("");
+
+//   let popUpContent = null;
+
+//   function handleClickPageCircle(event) {
+//     setShowPopUp(true);
+//     setActivePopUp(event.target.id);
+//   }
+
+//   function handleClosePopUp() {
+//     setShowPopUp(false);
+//     setActivePopUp("");
+//   }
+
+//   return (
+//     <>
+//       <div className="pages">
+//         <PagesCircle
+//           text="Work Experience"
+//           id="experience"
+//           onClick={handleClickPageCircle}
+//           activePopUp={activePopUp}
+//         />
+//         <PagesCircle
+//           text="Skills and Coursework"
+//           id="skills"
+//           onClick={handleClickPageCircle}
+//           activePopUp={activePopUp}
+//         />
+//         <PagesCircle
+//           text="Campus Involvement"
+//           id="involvement"
+//           onClick={handleClickPageCircle}
+//           activePopUp={activePopUp}
+//         />
+//         <PagesCircle
+//           text="Full Resume"
+//           id="fullresume"
+//           onClick={handleClickPageCircle}
+//           link={
+//             "https://drive.google.com/file/d/1YBhGyd2uvTaqZp40geLOAvHsb8U0BF2_/view?usp=drive_link"
+//           }
+//           activePopUp={activePopUp}
+//         />
+//         {popUpContent}
+//       </div>
+//       {showPopUp && (
+//         <PopUp activePopUp={activePopUp} onClick={handleClosePopUp} />
+//       )}
+//     </>
+//   );
+// }
--- src/components/PagesCircle.js
+++ src/components/PagesCircle.js
@@ -1,18 +1,16 @@
 export function PagesCircle({ text, onClick, id, activePopUp, link }) {
-    console.log("activePopUp:", activePopUp);
-    console.log("id:", id);
 
-    const handleClick = () => {
-        if (link) {
-          window.open(link);
-        }
-      };
+    // const handleClick = () => {
+    //     if (link) {
+    //       window.open(link);
+    //     }
+    //   };
   
     return (
       <div
         className={activePopUp === id ? "pages-circle-highlight" : "pages-circle"}
         id={id}
-        onClick={link ? handleClick : onClick}
+        onClick={onClick}
       >
         {text}
       </div>
